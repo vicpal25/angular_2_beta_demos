420c66908d35eefd3a4d0a381635611a242832db
Victor Plama <vpalma@tribpub.com>
2016-03-31 15:06:53 -0700

    keysearch component introduction

--- ng2demos/app/components/searchgrid/searchgrid.js
+++ ng2demos/app/components/searchgrid/searchgrid.js
@@ -1,4 +1,4 @@
-System.register(['angular2/core', './column', './sortable'], function(exports_1, context_1) {
+System.register(['angular2/core', './column', './sortable', './keysearch'], function(exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
@@ -10,7 +10,7 @@ System.register(['angular2/core', './column', './sortable'], function(exports_1,
     var __metadata = (this && this.__metadata) || function (k, v) {
         if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
     };
-    var core_1, column_1, sortable_1;
+    var core_1, column_1, sortable_1, keysearch_1;
     var SearchGrid;
     return {
         setters:[
@@ -22,21 +22,24 @@ System.register(['angular2/core', './column', './sortable'], function(exports_1,
             },
             function (sortable_1_1) {
                 sortable_1 = sortable_1_1;
+            },
+            function (keysearch_1_1) {
+                keysearch_1 = keysearch_1_1;
             }],
         execute: function() {
             SearchGrid = (function () {
                 function SearchGrid() {
                     this.sorter = new sortable_1.Sortable();
                     this.columns = [
-                        new column_1.Column('newsletter', 'First Name'),
-                        new column_1.Column('newsletter', 'Last Name'),
-                        new column_1.Column('age', 'Age')
+                        new column_1.Column('newsletter', 'Name'),
+                        new column_1.Column('status', 'Status'),
+                        new column_1.Column('newsleterid', 'Newsletter ID')
                     ];
                     this.rows = [
-                        { newsletter: 'Joe', lastName: 'Jackson', age: 20 },
-                        { newsletter: 'Peter', lastName: 'Smith', age: 30 },
-                        { newsletter: 'Jane', lastName: 'Doe', age: 50 },
-                        { newsletter: 'Tim', lastName: 'Smith', age: 80 }
+                        { newsletter: 'South Sentinel Midday Report', status: 'Production', newsleterid: 2089098223 },
+                        { newsletter: 'Los Angeles Times Breaking News', status: 'Test', newsleterid: 2089098022 },
+                        { newsletter: 'South Florida Local Hollywood', status: 'Production', newsleterid: 2089094504 },
+                        { newsletter: 'South Florida Travel Unraveled', status: 'Test', newsleterid: 2089098061 }
                     ];
                 }
                 SearchGrid.prototype.sort = function (key) {
@@ -46,7 +49,8 @@ System.register(['angular2/core', './column', './sortable'], function(exports_1,
                     core_1.Component({
                         selector: 'grid',
                         properties: ['rows: rows', 'columns: columns'],
-                        templateUrl: './app/templates/searchgrid.component.html'
+                        templateUrl: './app/templates/searchgrid.component.html',
+                        directives: [keysearch_1.KeySearch]
                     }), 
                     __metadata('design:paramtypes', [])
                 ], SearchGrid);
--- ng2demos/app/components/searchgrid/searchgrid.ts
+++ ng2demos/app/components/searchgrid/searchgrid.ts
@@ -4,12 +4,15 @@
 import {Component, View} from 'angular2/core';
 import {Column} from './column';
 import {Sortable} from './sortable';
+import {KeySearch} from './keysearch';
 
 @Component({
     selector: 'grid',
     properties: ['rows: rows','columns: columns'],
-    templateUrl: './app/templates/searchgrid.component.html'
+    templateUrl: './app/templates/searchgrid.component.html',
+    directives: [KeySearch]
 })
+
 
 export class SearchGrid {
 
@@ -17,18 +20,18 @@ export class SearchGrid {
     rows:Array<any>;
 
     constructor() {
-      
+
          this.columns = [
-             new Column('newsletter','First Name'),
-             new Column('newsletter','Last Name'),
-             new Column('age','Age')
+             new Column('newsletter','Name'),
+             new Column('status','Status'),
+             new Column('newsleterid','Newsletter ID')
          ];
 
          this.rows = [
-           {newsletter:'Joe',lastName:'Jackson',age:20},
-           {newsletter:'Peter',lastName:'Smith',age:30},
-           {newsletter:'Jane',lastName:'Doe',age:50},
-           {newsletter:'Tim',lastName:'Smith',age:80}
+           {newsletter:'South Sentinel Midday Report',status:'Production',newsleterid:2089098223},
+           {newsletter:'Los Angeles Times Breaking News',status:'Test',newsleterid:2089098022},
+           {newsletter:'South Florida Local Hollywood',status:'Production',newsleterid:2089094504},
+           {newsletter:'South Florida Travel Unraveled',status:'Test',newsleterid:2089098061}
          ]
 
     }
